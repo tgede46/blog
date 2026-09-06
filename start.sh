7caf1cbb6f8a065994ba522c31b572ef549a359e
tgede46 <gedeonkpara@gmail.com>
2026-09-06 20:04:44 +0000

    fix: add start.sh for Render and simplify render.yaml

---
+++ start.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+cd backend
+
+alembic upgrade head
+
+exec uvicorn app.main:app \
+    --host 0.0.0.0 \
+    --port "${PORT:-8000}" \
+    --proxy-headers \
+    --forwarded-allow-ips="*"
